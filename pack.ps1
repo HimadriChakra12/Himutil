c5916dbdc09c37ed29fd9da944e1de3cebef9763
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-20 21:40:01 +0600

    HimUtil

---
+++ pack.ps1
@@ -0,0 +1,31 @@
+writeh "  
+1. Chocolatey
+2. Scoop            
+3. Winget
+"
+$choicectt = readh "Choose"
+switch ($choicectt){
+    Set-ExecutionPolicy -ExecutionPolicy remotesigned
+    1{
+        iwr -useb "https://community.chocolatey.org/install.ps1" | iex
+    }
+    2{
+        startp powershell -ArgumentList '-noexit -NoProfile -ExecutionPolicy Bypass -Command "irm -Uri https://get.scoop.sh | iex"'  
+    }
+    3{
+        startp powershell -ArgumentList '-noexit -NoProfile -ExecutionPolicy Bypass -Command "wsreset -i"' -wait
+            while ($true) {
+                $store = Get-AppxPackage -Name "Microsoft.WindowsStore" -ErrorAction SilentlyContinue
+                    if ($store) {
+                        Write-Host "Microsoft Store is installed."
+                            msg * "Microsoft Store is installed."
+                            [console]::beep(1000, 300)
+                            start-process "https://apps.microsoft.com/detail/9nblggh4nns1?hl=en-US&gl=US"
+                                break
+                    } else {
+                        Write-Host "Microsoft Store not found. Retrying in 5 seconds..."
+                            msg * "Microsoft Store is not installed. Retrying in 5 seconds..."
+                            Start-Sleep -Seconds 5
+                    }
+            }   
+    }
