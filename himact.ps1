e25dcd27a55a5267bf59eba63ffd4e6305de5b30
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-25 17:53:29 +0600

    HimAct

--- himact.ps1
+++ himact.ps1
@@ -1,13 +1,23 @@
-# Configuration
-$cmdUrl = "https://raw.githubusercontent.com/HimadriChakra12/Himutil/refs/heads/master/Activator/himact.cmd"  # Replace with your actual raw URL
+$osInfo = Get-CimInstance Win32_OperatingSystem
+$version = [System.Version]$osInfo.Version
+$buildNumber = [int]$osInfo.BuildNumber
+
+if ($version.Major -eq 10 -and $buildNumber -lt 22000) {
+    Write-Host "Windows 10 detected"
+        Write-Host "Running Windows 10 specific command..."
+        $cmdUrl = "https://raw.githubusercontent.com/HimadriChakra12/Himutil/refs/heads/master/Activator/10act.cmd"
+
+}
+elseif ($version.Major -eq 10 -and $buildNumber -ge 22000) {
+    Write-Host "Windows 11 detected"
+        Write-Host "Running Windows 11 specific command..."
+        $cmdUrl = "https://raw.githubusercontent.com/HimadriChakra12/Himutil/refs/heads/master/Activator/11act.cmd"
+}
+else {
+    Write-Host "Unsupported Windows version: $version (Build $buildNumber)"
+}
 $cmdFile = "$env:LOCALAPPDATA\Temp\himact.cmd"
-
-# Download the CMD file
 Invoke-WebRequest -Uri $cmdUrl -OutFile $cmdFile
-
-# Execute the CMD file
 start-process "$cmdFile" -wait
-# Optional delay before cleanup if needed
 Start-Sleep -Seconds 1
-# Clean up
 Remove-Item -Path $cmdFile -Force
