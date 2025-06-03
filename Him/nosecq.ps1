30e3ff05727bb908a1ec5e31caf419207a695f75
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-06-03 03:12:06 -0700

    Remove Act

--- Him/nosecq.ps1
+++
@@ -1,8 +0,0 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
-Set-MpPreference -DisableBehaviorMonitoring $true
-Set-MpPreference -DisableBlockAtFirstSeen $true
-Set-MpPreference -DisableIOAVProtection $true
-Set-MpPreference -DisableScriptScanning $true
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1 -Force
-
-Write-Host "Windows Defender protections have been disabled (some may reactivate on reboot)."
